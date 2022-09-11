c8093b6a0de2fc9527543287d5bc00fe9cda6302
Seven <turtl3.seven@gmail.com>
2022-09-11 18:10:52 +0200

    finsihed

---
+++ receipt/index.php
@@ -0,0 +1,9 @@
+<?php include("../header.php");?>
+<div id="content" class="main-container d-flex flex-column justify-content-start align-items-center">
+    <div class="col-8">
+        asdasd
+    </div>
+</div>
+</body>
+<script>
+</script>
